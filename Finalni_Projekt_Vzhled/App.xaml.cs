dc3195dbefd15a963262692e1d8a719884097188
skorpja <skorpja@LAB220-10>
2025-05-06 15:01:24 +0200

    Přidejte soubory projektu.

---
+++ Finalni_Projekt_Vzhled/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace Finalni_Projekt_Vzhled
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
---
+++ Finalni_Projekt_Vzhled/MainWindow.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Finalni_Projekt_Vzhled
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private void Continue1_Click(object sender, RoutedEventArgs e)
+        {
+            Alkohol_detaily.Visibility = Visibility.Visible;
+        }
+
+        private void Continue2_Click(object sender, RoutedEventArgs e)
+        {
+            Vyber_alk.Visibility = Visibility.Visible;
+        }
+    
+
+        private void Gender_Checked(object sender, RoutedEventArgs e)
+        {
+            if (sender == CheckBoxMuz && CheckBoxMuz.IsChecked == true)
+                CheckBoxZena.IsChecked = false;
+            else if (sender == CheckBoxZena && CheckBoxZena.IsChecked == true)
+                CheckBoxMuz.IsChecked = false;
+        }
+
+        private void Gender_Unchecked(object sender, RoutedEventArgs e)
+        {
+            // Volitelně můžeš doplnit logiku
+        }
+    }
+}
